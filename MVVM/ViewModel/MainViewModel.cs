f920b89f6f029118c6871e6be824be101f266c14
Lucas Estefano <lucccasestefano_@hotmail.com>
2024-05-25 18:59:27 +1000

    New files of server.

---
+++ MVVM/ViewModel/MainViewModel.cs
@@ -0,0 +1,22 @@
+﻿using ChatClient.MVVM.Core;
+using ChatClient.Net;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ChatClient.MVVM.ViewModel
+{
+    internal class MainViewModel
+    {
+        public RelayCommand ConnectToServerCommand { get; set; }
+
+        private Server server;
+        public MainViewModel() { 
+            server = new Server();
+            ConnectToServerCommand = new RelayCommand(o => server.ConnectToServer());
+        
+        }
+    }
+}
--- Net/Server.cs
+++ Net/Server.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net.Sockets;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -8,5 +9,18 @@ namespace ChatClient.Net
 {
     internal class Server
     {
+        TcpClient client;
+
+        public Server()
+        {
+            client = new TcpClient();
+        }
+
+        public void ConnectToServer()
+        {
+            if(!client.Connected) {
+                client.Connect("127.0.0.1", 7891);            
+            }
+        }
     }
 }
